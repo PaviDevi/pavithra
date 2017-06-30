259f51b236d02c53252d3c04df06eb59d3cee834
PaviDevi <pavithradevi.pg@gmail.com>
2017-06-30 12:11:47 +0530

    Create reverse.c

---
+++ reverse.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int num,rn=0,reminder;
+clrscr();
+printf("enter the number");
+scanf("%d",&num);
+while(num!=0)
+{
+reminder=n%10;
+rn=rn*10+reminder;
+num\10;
+}
+printf("rn=%d",rn);
+getch();
+return 0;
+}
