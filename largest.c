05fcbdd60227be1a89816cc8bf7700d02831c929
PaviDevi <pavithradevi.pg@gmail.com>
2017-07-01 10:34:03 +0530

    Create largest.c

---
+++ largest.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int num1,num2,num3;
+printf("enter the three number:");
+scanf("%d %d %d",&num1,&num2,&num3);
+if(num1>=num2 && num1>=num3)
+printf("%d is the largetst number",num1);
+if(num2>=num1 && num2>=num3)
+printf(%d is the largest number",num2);
+if(num3>=num1 && num3>=num2)
+printf(%d is the largest number",num3);
+return 0;
+}
