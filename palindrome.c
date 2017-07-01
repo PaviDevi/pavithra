50e1dc3c52f857a0aa7e7f7cbb1cff71d8d41dc4
PaviDevi <pavithradevi.pg@gmail.com>
2017-07-01 11:44:51 +0530

    Create palindrome.c

---
+++ palindrome.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,reverse=0;
+clrscr();
+scanf("%d",&n);
+while(n!=0)
+{
+reminder=n%10;
+reverse=reverse*10+reminder;
+n/=10;
+}
+n=reverse integer;
+{
+printf("palindrome",reverse);
+else
+printf("not palindrome',reverse);
+}
+getch();
+}
